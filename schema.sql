ab49e65dd9e01aaff5b9b41875a029901ea899a0
ldg118 <bcvfkd@gmail.com>
2025-05-26 15:39:09 +0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL,
-  role TEXT DEFAULT 'user' -- 'admin' or 'user'
+  role TEXT DEFAULT 'user'
 );
 
 -- 注册商表
